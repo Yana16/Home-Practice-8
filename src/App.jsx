e9ef24ecff15837b11498979950e0d3cd3b32835
Yana Vozniuk <yana.vozniuk@ukr.net>
2023-03-19 14:39:41 +0200

    5
    5

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,38 @@
-import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Suspense } from 'react';
 
 // import AuthLayout from './modules/AuthLayout/AuthLayout';
 import Navbar from '../src/modules/Navbar/Navbar';
-import UserRoutes from '../src/UserRoutes';
+import LoginPage from '../src/pages/LoginPage/LoginPage';
+import RegisterPage from '../src/pages/RegisterPage/RegisterPage';
 
 import store from '../src/redux/store';
 import persistor from '../src/redux/store';
 
 // import './App.css';
 
-function App() {
+const App = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         {/* <AuthLayout> */}
-        <BrowserRouter>
+
+        <div>
           <Navbar />
-          <UserRoutes />
-        </BrowserRouter>
-        {/* </AuthLayout> */}
+          <Suspense fallback={<p>...Loading page</p>}>
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+
+              {/* </AuthLayout> */}
+            </Routes>
+          </Suspense>
+        </div>
       </PersistGate>
     </Provider>
   );
-}
+};
 
 export default App;
